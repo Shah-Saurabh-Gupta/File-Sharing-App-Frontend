c385e7006a2df298e5d6ab3dffe04fae26d7dfc6
Saurabh Gupta <saurabhg@uvxcel.com>
2021-09-25 16:45:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const dropZone = document.querySelector(".drop-zone");
+const browseBtn = document.querySelector(".browseBtn");
+const fileInput = document.querySelector("#fileInput");
+
+//only 3000 port allowed
+const host = "https://innShare.herokuapp.com/";
+const uploadURL = `${host}api/files`;
+
+dropZone.addEventListener("dragover", (e) => {
+    //console.log("dragging");
+    e.preventDefault();
+    if (!dropZone.classList.contains("dragged")) {
+        dropZone.classList.add("dragged");
+    }
+});
+
+dropZone.addEventListener("dragleave", () => {
+    dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
+    const files = e.dataTransfer.files
+    //console.table(files);
+    if (files.length) {
+        fileInput.files = files;
+        uploadFile();
+    }
+});
+
+fileInput.addEventListener("change", () => {
+    uploadFile();
+})
+
+browseBtn.addEventListener("click", () => {
+    fileInput.click();
+});
+
+const uploadFile = () => {
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append("myfile", file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        //console.log(xhr.readyState);
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.response);
+        }
+    };
+    xhr.open("POST", uploadURL);
+    xhr.send(formData)
+};
