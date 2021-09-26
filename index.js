dc30849826399fc4eb705e7a54fae6f7397960d3
Saurabh Gupta <saurabhg@uvxcel.com>
2021-09-26 19:00:27 +0530

    Link Generation and Copying

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const dropZone = document.querySelector(".drop-zone");
 const browseBtn = document.querySelector(".browseBtn");
 const fileInput = document.querySelector("#fileInput");
 
+const progressContainer = document.querySelector(".progress-container");
+const bgProgress = document.querySelector(".bg-progress");
+const progressBar = document.querySelector(".progress-bar");
+const percentDiv = document.querySelector("#percent");
+
+const sharingContainer = document.querySelector(".sharing-container");
+const fileURLinput = document.querySelector("#fileURL");
+const copyBtn = document.querySelector("#cpoyBtn");
+
 //only 3000 port allowed
 const host = "https://innShare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
@@ -37,18 +46,44 @@ browseBtn.addEventListener("click", () => {
     fileInput.click();
 });
 
+copyBtn.addEventListener("click", () => {
+    fileURLinput.select();
+    document.execCommand("copy");
+});
+
 const uploadFile = () => {
+    progressContainer.style.display = "block";
     const file = fileInput.files[0];
     const formData = new FormData();
     formData.append("myfile", file);
 
     const xhr = new XMLHttpRequest();
+    //
     xhr.onreadystatechange = () => {
         //console.log(xhr.readyState);
         if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log(xhr.response);
+            //console.log(xhr.response);
+            showLink(JSON.parse(xhr.response));
         }
     };
+    xhr.upload.onprogress = updateProgress;
+
     xhr.open("POST", uploadURL);
-    xhr.send(formData)
+    xhr.send(formData);
+};
+
+const updateProgress = (e) => {
+    //console.log(e);
+    const percent = Math.round((e.loaded / e.total) * 100);
+    //console.log(percent);
+    bgProgress.style.width = `${percent}%`;
+    percentDiv.innerText = percent;
+    progressBar.style.transform = `scaleX(${percent / 100})`;
+};
+
+const showLink = ({ file: url }) => {
+    //console.log(url);
+    progressContainer.style.display = "none";
+    sharingContainer.style.display = "block";
+    fileURLinput.value = url;
 };
