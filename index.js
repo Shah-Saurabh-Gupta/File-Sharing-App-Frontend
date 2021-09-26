55f41797669a9b35aa8af922176d9d4ce4ddfff9
Saurabh Gupta <saurabhg@uvxcel.com>
2021-09-26 20:00:36 +0530

    Email Form

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ const sharingContainer = document.querySelector(".sharing-container");
 const fileURLinput = document.querySelector("#fileURL");
 const copyBtn = document.querySelector("#cpoyBtn");
 
+const emailForm = document.querySelector("#emailForm");
+
 //only 3000 port allowed
 const host = "https://innShare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
+const emailURL = `${host}api/files/send`;
 
 dropZone.addEventListener("dragover", (e) => {
     //console.log("dragging");
@@ -63,7 +66,7 @@ const uploadFile = () => {
         //console.log(xhr.readyState);
         if (xhr.readyState === XMLHttpRequest.DONE) {
             //console.log(xhr.response);
-            showLink(JSON.parse(xhr.response));
+            onUploadSuccess(JSON.parse(xhr.response));
         }
     };
     xhr.upload.onprogress = updateProgress;
@@ -81,9 +84,42 @@ const updateProgress = (e) => {
     progressBar.style.transform = `scaleX(${percent / 100})`;
 };
 
-const showLink = ({ file: url }) => {
+const onUploadSuccess = ({ file: url }) => {
     //console.log(url);
+    fileInput.value = "";
+    emailForm[2].removeAttribute("disabled", "true");
     progressContainer.style.display = "none";
     sharingContainer.style.display = "block";
     fileURLinput.value = url;
 };
+
+emailForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // stop submission
+  
+    // disable the button
+    emailForm[2].setAttribute("disabled", "true");
+    emailForm[2].innerText = "Sending";
+  
+    const url = fileURL.value;
+  
+    const formData = {
+      uuid: url.split("/").splice(-1, 1)[0],
+      emailTo: emailForm.elements["to-email"].value,
+      emailFrom: emailForm.elements["from-email"].value,
+    };
+    //console.log(formData);
+    fetch(emailURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          showToast("Email Sent");
+          sharingContainer.style.display = "none"; // hide the box
+        }
+      });
+  });
