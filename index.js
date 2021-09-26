e2b00326d11a815c2569b2f7755d87e6b7bbdf89
Saurabh Gupta <saurabhg@uvxcel.com>
2021-09-26 21:03:19 +0530

    Adding Toast Button

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ const copyBtn = document.querySelector("#cpoyBtn");
 
 const emailForm = document.querySelector("#emailForm");
 
+const toast = document.querySelector(".toast");
+
+
 //only 3000 port allowed
 const host = "https://innShare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
 const emailURL = `${host}api/files/send`;
+
+const maxAllowedFileSize = 100 * 1024 * 1024; //100mb
 
 dropZone.addEventListener("dragover", (e) => {
     //console.log("dragging");
@@ -52,11 +57,24 @@ browseBtn.addEventListener("click", () => {
 copyBtn.addEventListener("click", () => {
     fileURLinput.select();
     document.execCommand("copy");
+    showToast("Link copied");
 });
 
 const uploadFile = () => {
-    progressContainer.style.display = "block";
+
+    if (fileInput.files.length > 1) {
+        resetFileInput();
+        showToast("Only upload 1 files at a time");
+        return;
+    }
     const file = fileInput.files[0];
+    if (file.size > maxAllowedFileSize) {
+        resetFileInput();
+        showToast("File size greater than 100mb");
+        return;
+    }
+
+    progressContainer.style.display = "block";
     const formData = new FormData();
     formData.append("myfile", file);
 
@@ -70,6 +88,11 @@ const uploadFile = () => {
         }
     };
     xhr.upload.onprogress = updateProgress;
+
+    xhr.upload.onerror = () => {
+        resetFileInput();
+        showToast(`Error in upload: ${xhr.statusTest}`);
+    }
 
     xhr.open("POST", uploadURL);
     xhr.send(formData);
@@ -86,40 +109,55 @@ const updateProgress = (e) => {
 
 const onUploadSuccess = ({ file: url }) => {
     //console.log(url);
-    fileInput.value = "";
+    resetFileInput();
     emailForm[2].removeAttribute("disabled", "true");
     progressContainer.style.display = "none";
     sharingContainer.style.display = "block";
     fileURLinput.value = url;
 };
 
+const resetFIleInput = () =>{
+    fileInput.value = "";
+}
+
 emailForm.addEventListener("submit", (e) => {
     e.preventDefault(); // stop submission
-  
+
     // disable the button
     emailForm[2].setAttribute("disabled", "true");
     emailForm[2].innerText = "Sending";
-  
+
     const url = fileURL.value;
-  
+
     const formData = {
-      uuid: url.split("/").splice(-1, 1)[0],
-      emailTo: emailForm.elements["to-email"].value,
-      emailFrom: emailForm.elements["from-email"].value,
+        uuid: url.split("/").splice(-1, 1)[0],
+        emailTo: emailForm.elements["to-email"].value,
+        emailFrom: emailForm.elements["from-email"].value,
     };
     //console.log(formData);
     fetch(emailURL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          showToast("Email Sent");
-          sharingContainer.style.display = "none"; // hide the box
-        }
-      });
-  });
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.success) {
+                showToast("Email Sent");
+                sharingContainer.style.display = "none"; // hide the box
+                showToast("Email Sent");
+            }
+        });
+});
+
+let toastTimer;
+const showToast = (msg) => {
+    toast.innerText = msg;
+    toast.style.transform = "translate(-50%, 0)";
+    clearTimeout(toastTimer);
+    toastTimer = setTimeout(() => {
+        toast.style.transform = "translate(-50%, 60px)";
+    }, 2000);
+}
